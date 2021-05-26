0dc61f28cc8b4bb4aaec611fa2f814d8c854ccf2
Kenneth Hunter <kennethdhunter@gmail.com>
2021-05-25 18:08:47 -0600

    Added layer 2 filter example to addrule!

--- addrule.cpp
+++ addrule.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdint-uintn.h>
 #include <linux/if_ether.h>
+#include <netinet/ether.h>
 #include <linux/netfilter/nf_tables_compat.h>
 #include <vector>
 #include <arpa/inet.h>
@@ -97,33 +98,56 @@ int main()
   // rule.add_expression(&icmp_cmp);
   // rule.add_expression(&counter);
   // rule.add_expression(&reject);
-  meta_expression mexpr(NFT_META_IIFTYPE, NFT_REG_1);
-  uint32_t daddr = offsetof(ethhdr, h_dest);
-  uint32_t mproto = IPPROTO_ETHERNET;
-  comparison_expression mcmp(NFT_REG_1, NFT_CMP_EQ, &mproto, sizeof(mproto));
-  meta_expression meta_expr(NFT_META_IIFNAME, NFT_REG_3);
-  const char* iifname = "enp3s0";
-  meta_expression out_meta_expr(NFT_META_OIFNAME, NFT_REG_4);
-  const char* oifname = "enp3s0";
-  comparison_expression oifcmp(NFT_REG_4, NFT_CMP_EQ, oifname, strlen(oifname));
-  comparison_expression meta_cmp(NFT_REG_3, NFT_CMP_EQ, iifname, strlen(iifname));
-  meta_expression ether_link(NFT_META_IIFTYPE, NFT_REG_2);
-  uint32_t ether_type = 0x01;
-  comparison_expression ether_cmp(NFT_REG_2, NFT_CMP_EQ, &ether_type, sizeof(ether_type));
-  payload_expression ether_payload(NFT_PAYLOAD_LL_HEADER, NFT_REG_2, offsetof(ethhdr, h_source), sizeof(unsigned char[6]));
-  
-  comparison_expression mac_cmp(NFT_REG_2, NFT_CMP_EQ, )
 
-  rule.add_expression(&mexpr);
-  rule.add_expression(&mcmp);
-  rule.add_expression(&meta_expr);
-  rule.add_expression(&meta_cmp);
-  rule.add_expression(&out_meta_expr);
-  rule.add_expression(&oifcmp);
-  rule.add_expression(&ether_link);
-  rule.add_expression(&ether_cmp);
-  rule.add_expression(&ether_payload);
+  //Complex rule example
+  // meta_expression mexpr(NFT_META_IIFTYPE, NFT_REG_1);
+  // uint32_t daddr = offsetof(ethhdr, h_dest);
+  // uint32_t mproto = IPPROTO_ETHERNET;
+  // comparison_expression mcmp(NFT_REG_1, NFT_CMP_EQ, &mproto, sizeof(mproto));
+  // meta_expression meta_expr(NFT_META_IIFNAME, NFT_REG_3);
+  // const char* iifname = "enp3s0";
+  // meta_expression out_meta_expr(NFT_META_OIFNAME, NFT_REG_4);
+  // const char* oifname = "enp3s0";
+  // comparison_expression oifcmp(NFT_REG_4, NFT_CMP_EQ, oifname, strlen(oifname));
+  // comparison_expression meta_cmp(NFT_REG_3, NFT_CMP_EQ, iifname, strlen(iifname));
+  // meta_expression ether_link(NFT_META_IIFTYPE, NFT_REG_2);
+  // uint32_t ether_type = 0x01;
+  // comparison_expression ether_cmp(NFT_REG_2, NFT_CMP_EQ, &ether_type, sizeof(ether_type));
+  // payload_expression ether_payload(NFT_PAYLOAD_LL_HEADER, NFT_REG_2, offsetof(ethhdr, h_source), sizeof(ether_addr));
+  // ether_addr* saddr = ether_aton("aa:cc:ee:aa:cc:ee");
+  // comparison_expression mac_cmp(NFT_REG_2, NFT_CMP_EQ, saddr, sizeof(*saddr));
+
+  // rule.add_expression(&mexpr);
+  // rule.add_expression(&mcmp);
+  // rule.add_expression(&meta_expr);
+  // rule.add_expression(&meta_cmp);
+  // rule.add_expression(&out_meta_expr);
+  // rule.add_expression(&oifcmp);
+  // rule.add_expression(&ether_link);
+  // rule.add_expression(&ether_cmp);
+  // rule.add_expression(&ether_payload);
+  // //rule.add_expression(&match);
+  // rule.add_expression(&mac_cmp);
+
+  //Layer 2 match on dest
+  meta_expression eth_meta(NFT_META_IIFTYPE, NFT_REG_1);
+  uint32_t ether = 0x01;
+  comparison_expression eth_cmp(NFT_REG_1, NFT_CMP_EQ, &ether, sizeof(ether));
+  payload_expression eth_payload(NFT_PAYLOAD_LL_HEADER, NFT_REG_1, offsetof(ethhdr, h_dest), sizeof(ether_addr));
+  ether_addr* daddr = ether_aton("d4:d2:52:8d:97:d5");
+  comparison_expression mac_cmp(NFT_REG_1, NFT_CMP_EQ, daddr, sizeof(ether_addr));
+  payload_expression src_payload(NFT_PAYLOAD_LL_HEADER, NFT_REG_2, offsetof(ethhdr, h_source), sizeof(ether_addr));
+  ether_addr* saddr = ether_aton("d4:d2:52:8d:97:d5");
+  comparison_expression src_mac_cmp(NFT_REG_2, NFT_CMP_EQ, saddr, sizeof(ether_addr));
+  counter_expression counter;
+
+  rule.add_expression(&eth_meta);
+  rule.add_expression(&eth_cmp);
+  rule.add_expression(&eth_payload);
   rule.add_expression(&mac_cmp);
+  rule.add_expression(&src_payload);
+  rule.add_expression(&src_mac_cmp);
+  rule.add_expression(&counter);
   rule.build_nlmsg_payload(nlh);
   mnl_nlmsg_batch_next(batch);
   make_batch_header(reinterpret_cast<char*>(mnl_nlmsg_batch_current(batch)), NFNL_MSG_BATCH_END, seq++);
